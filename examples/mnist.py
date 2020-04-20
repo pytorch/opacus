1e925fd78a6a0fdc07428364c656c90a81c5391e
Ilya Mironov <imironov@fb.com>
2020-04-20 14:40:12 -0700

    Changing the convnet + changing defaults
    Summary:
    + Changing the padding and adding a dense layer to the convnet
    + Changing defaults to a converging network
    + Cosmetic changes
    
    Reviewed By: Darktex, karthikprasad
    
    Differential Revision: D21060082
    
    fbshipit-source-id: 39b0474d6e2dc79f3b32715d824240cbcc5d2940

--- examples/mnist.py
+++ examples/mnist.py
@@ -8,6 +8,8 @@ Runs MNIST training with differential privacy.
 
 import argparse
 
+import numpy as np
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -17,21 +19,28 @@ from torchvision import datasets, transforms
 from tqdm import tqdm
 
 
+# Precomputed characteristics of the MNIST dataset
+MNIST_MEAN = 0.1307
+MNIST_STD = 0.3081
+
+
 class SampleConvNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 16, 8, 2)
+        self.conv1 = nn.Conv2d(1, 16, 8, 2, padding=3)
         self.conv2 = nn.Conv2d(16, 32, 4, 2)
-        self.fc1 = nn.Linear(32 * 3 * 3, 10)
+        self.fc1 = nn.Linear(32 * 4 * 4, 32)
+        self.fc2 = nn.Linear(32, 10)
 
     def forward(self, x):
         # x of shape [B, 1, 28, 28]
-        x = F.relu(self.conv1(x))  # -> [B, 16, 11, 11]
-        x = F.max_pool2d(x, 2, 1)  # -> [B, 16, 10, 10]
-        x = F.relu(self.conv2(x))  # -> [B, 32, 4, 4]
-        x = F.max_pool2d(x, 2, 1)  # -> [B, 32, 3, 3]
-        x = x.view(-1, 32 * 3 * 3)  # -> [B, 32*3*3]
-        x = self.fc1(x)  # -> [B, 10]
+        x = F.relu(self.conv1(x))   # -> [B, 16, 14, 14]
+        x = F.max_pool2d(x, 2, 1)   # -> [B, 16, 13, 13]
+        x = F.relu(self.conv2(x))   # -> [B, 32, 5, 5]
+        x = F.max_pool2d(x, 2, 1)   # -> [B, 32, 4, 4]
+        x = x.view(-1, 32 * 4 * 4)  # -> [B, 512]
+        x = F.relu(self.fc1(x))     # -> [B, 32]
+        x = self.fc2(x)             # -> [B, 10]
         return x
 
     def name(self):
@@ -41,6 +50,7 @@ class SampleConvNet(nn.Module):
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     criterion = nn.CrossEntropyLoss()
+    losses = []
     for _batch_idx, (data, target) in enumerate(tqdm(train_loader)):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
@@ -48,16 +58,17 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss = criterion(output, target)
         loss.backward()
         optimizer.step()
+        losses.append(loss.item())
 
     if not args.disable_dp:
         epsilon, best_alpha = optimizer.privacy_engine.get_privacy_spent(args.delta)
         print(
             f"Train Epoch: {epoch} \t"
-            f"Loss: {loss.item():.6f} "
-            f"(Ɛ = {epsilon}, 𝛿 = {args.delta}) for α = {best_alpha}"
+            f"Loss: {np.mean(losses):.6f} "
+            f"(ε = {epsilon:.2f}, δ = {args.delta}) for α = {best_alpha}"
         )
     else:
-        print(f"Train Epoch: {epoch} \t" f"Loss: {loss.item():.6f} ")
+        print(f"Train Epoch: {epoch} \t Loss: {np.mean(losses):.6f}")
 
 
 def test(args, model, device, test_loader):
@@ -78,7 +89,7 @@ def test(args, model, device, test_loader):
     test_loss /= len(test_loader.dataset)
 
     print(
-        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n".format(
             test_loss,
             correct,
             len(test_loader.dataset),
@@ -110,7 +121,7 @@ def main():
         "-n",
         "--epochs",
         type=int,
-        default=14,
+        default=10,
         metavar="N",
         help="number of epochs to train (default: 14)",
     )
@@ -125,9 +136,9 @@ def main():
     parser.add_argument(
         "--lr",
         type=float,
-        default=1.0,
+        default=.1,
         metavar="LR",
-        help="learning rate (default: 1.0)",
+        help="learning rate (default: .1)",
     )
     parser.add_argument(
         "--sigma",
@@ -161,7 +172,7 @@ def main():
         "--save-model",
         action="store_true",
         default=False,
-        help="For Saving the current Model",
+        help="Save the trained model (default: false)",
     )
     parser.add_argument(
         "--disable-dp",
@@ -179,13 +190,15 @@ def main():
     device = torch.device(args.device)
 
     kwargs = {"num_workers": 1, "pin_memory": True}
+
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST(
             args.data_root,
             train=True,
             download=True,
             transform=transforms.Compose(
-                [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+                [transforms.ToTensor(),
+                transforms.Normalize((MNIST_MEAN,), (MNIST_STD,))]
             ),
         ),
         batch_size=args.batch_size,
@@ -197,7 +210,8 @@ def main():
             args.data_root,
             train=False,
             transform=transforms.Compose(
-                [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+                [transforms.ToTensor(),
+                transforms.Normalize((MNIST_MEAN,), (MNIST_STD,))]
             ),
         ),
         batch_size=args.test_batch_size,
@@ -205,8 +219,9 @@ def main():
         **kwargs,
     )
     run_results = []
-    for _run in range(1, args.n_runs + 1):
+    for _ in range(args.n_runs):
         model = SampleConvNet().to(device)
+
         optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0)
         if not args.disable_dp:
             privacy_engine = PrivacyEngine(
@@ -222,10 +237,14 @@ def main():
             train(args, model, device, train_loader, optimizer, epoch)
         run_results.append(test(args, model, device, test_loader))
 
-    run_results = torch.Tensor(run_results)
-    print(
-        f"Accuracy: {torch.mean(run_results).item()} ± {torch.std(run_results).item()}"
-    )
+    if len(run_results) > 1:
+        print("Accuracy averaged over {} runs: {:.2f}% ± {:.2f}%".format(
+            len(run_results),
+            np.mean(run_results) * 100,
+            np.std(run_results) * 100
+        )
+        )
+
     repro_str = (
         f"{model.name()}_{args.lr}_{args.sigma}_"
         f"{args.max_per_sample_grad_norm}_{args.batch_size}_{args.epochs}"
