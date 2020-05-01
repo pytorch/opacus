e1aaf82428b8a87a5868437c9eb9f79292174759
Jean-Marc Gervais <jm.gervais@free.fr>
2020-05-01 20:27:05 +0200

    Merge branch 'compute_privacy' of https://github.com/jmg-74/pytorch-dp into compute_privacy

--- torchdp/compute_dp_sgd_privacy.py
+++ torchdp/compute_dp_sgd_privacy.py
@@ -16,15 +16,13 @@ Example:
     --epochs=60 \
     --delta=1e-5
 The output states that DP-SGD with these parameters satisfies (2.92, 1e-5)-DP.
-
-In addition, there is a -o or --orders argument for a list of RDP orders
-(usually denoted alpha). All arguments have a default value, including -o.
 """
 import argparse
 import math
 from privacy_analysis import compute_rdp, get_privacy_spent
 
-def apply_dp_sgd_analysis(q, sigma, steps, orders, delta, printed=True):
+
+def apply_dp_sgd_analysis(q, sigma, steps, orders, delta):
     """Compute and print results of DP-SGD analysis.
 
     Args:
@@ -34,62 +32,45 @@ def apply_dp_sgd_analysis(q, sigma, steps, orders, delta, printed=True):
                the l2-sensitivity of the function to which it is added
         steps: the number of steps.
         orders: an array (or a scalar) of RDP orders.
-        printed: boolean, True (by default) to print results on stdout.
   """
     rdp = compute_rdp(q, sigma, steps, orders)
-    #  Slight adaptation from TF version, in which
+
+    #  Slight adaptation from TF version, in which
     # `get_privacy_spent()` has one more arguments and one more element in
     #  returned tuple, because it can also compute delta for a given epsilon
     #  (and not only compute epsilon for a targeted delta).
     eps, opt_order = get_privacy_spent(orders, rdp, delta=delta)
 
-    if printed:
-        print(
-            f"DP-SGD with\n\tsampling rate = {100 * q:.3g}% and"
-            f"\n\tnoise_multiplier = {sigma}"
-            f"\n\titerated over {steps} steps\n  satisfies "
-            f"differential privacy with\n\teps = {eps:.3g} "
-            f"and\n\tdelta = {delta}."
-            f"\nThe optimal RDP order is {opt_order}."
-        )
+    print(
+        f"DP-SGD with\n\tsampling rate = {100 * q:.3g}% and"
+        f"\n\tnoise_multiplier = {sigma}"
+        f"\n\titerated over {steps} steps\n  satisfies "
+        f"differential privacy with\n\teps = {eps:.3g} and\n\tdelta = {delta}."
+        f"\nThe optimal RDP order is {opt_order}."
+    )
 
-        if opt_order == max(orders) or opt_order == min(orders):
-            print(
-                "The privacy estimate is likely to be improved by expanding "
-                "the set of orders."
-            )
+    if opt_order == max(orders) or opt_order == min(orders):
+        print(
+            "The privacy estimate is likely to be improved by expanding "
+            "the set of orders."
+        )
 
     return eps, opt_order
 
 
-def compute_dp_sgd_privacy(n,
-                           batch_size,
-                           noise_multiplier,
-                           epochs,
-                           delta,
-                           orders=None,
-                           printed=True,
-):
-    """Compute epsilon based on the given hyperparameters.
-    """
-    if orders is None:
-        orders = ([1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 3.0, 3.5, 4.0, 4.5]
-            + list(range(5, 64))
-            + [128, 256, 512]
-    )
-
+def compute_dp_sgd_privacy(n, batch_size, noise_multiplier, epochs, delta):
+    """Compute epsilon based on the given hyperparameters."""
     q = batch_size / n  # q - the sampling ratio.
     if q > 1:
         raise ValueError("n must be larger than the batch size.")
+    orders = (
+        [1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 3.0, 3.5, 4.0, 4.5]
+        + list(range(5, 64))
+        + [128, 256, 512]
+    )
     steps = int(math.ceil(epochs * n / batch_size))
 
-    return apply_dp_sgd_analysis(q,
-                                noise_multiplier,
-                                steps,
-                                orders,
-                                delta,
-                                printed
-    )
+    return apply_dp_sgd_analysis(q, noise_multiplier, steps, orders, delta)
 
 
 def main():
@@ -130,32 +111,15 @@ def main():
         default=1e-5,
         help="Targeted delta (default: 1e-5)",
     )
-    parser.add_argument(
-        "-o",
-        "--orders",
-        type=str,
-        default=("[1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 3.0, 3.5, 4.0, 4.5, "
-            + str(list(range(5, 64))).strip('[]')
-            + "128, 256, 512]"
-        ),
-        help="List of orders (alpha values) for Rényi-DP evaluation. "
-             "A default list is provided. Else, write it quoted, e.g. "
-             "\"[1.25, 1.5, 1.75, 2.0, 3.0, 4.0, 5.0, 10.0]\"",
-    )
-
     args = parser.parse_args()
 
-    # From str to list of floats for orders
-    args.orders = [float(a) for a in args.orders.strip('[]').split(',')]
-
     print("=================================================================="
-          "\n* Script was called with those arguments:\n\t"
+          "\n* Script called with those arguments:\n\t"
           f"-n = --dataset-size = {args.dataset_size}\n\t"
           f"-b = --batch-size = {args.batch_size}\n\t"
           f"-s = --sigma = {args.sigma}\n\t"
           f"-e = --epochs = {args.epochs}\n\t"
-          f"-d = --delta = {args.delta}\n\t"
-          f"-o = --orders = {args.orders}\n\n"
+          f"-d = --delta = {args.delta}\n"
           "* Result is:",
           end="\n  "
     )
@@ -165,9 +129,9 @@ def main():
         args.sigma,
         args.epochs,
         args.delta,
-        args.orders,
     )
     print("==================================================================")
+
 
 if __name__ == "__main__":
     main()
