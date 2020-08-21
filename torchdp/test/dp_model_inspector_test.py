d451a7e1a752797caa0b7ce6c26864886baa830d
Igor Shilov <shilov@fb.com>
2020-08-21 05:58:55 -0700

    Extra tests for model inspector
    Summary:
    We were missing some important use cases in DPModelInspector tests.
    
    Added a few:
    
    - Model with a supported layer + independent parameter. Validation should fail if parameter is trainable and succeed if parameter is frozen
    - Number of groups in Conv2d
    - Explicitly test that we fail validation for unsupported layers
    
    All tests pass
    
    Reviewed By: Darktex, karthikprasad
    
    Differential Revision: D22818145
    
    fbshipit-source-id: 3e6a661f06793e75551a7fb5ca875b527d71e13b

--- torchdp/test/dp_model_inspector_test.py
+++ torchdp/test/dp_model_inspector_test.py
@@ -4,6 +4,7 @@
 
 import unittest
 
+import torch
 import torch.nn as nn
 from torchdp import dp_model_inspector as dp_inspector
 from torchdp.utils.module_modification import convert_batchnorm_modules
@@ -39,4 +40,55 @@ class dp_model_inspector_test(unittest.TestCase):
             inspector.validate(
                 nn.InstanceNorm1d(16, affine=True, track_running_stats=True)
             )
+        )
+
+    def test_extra_param(self):
+        inspector = dp_inspector.DPModelInspector(should_throw=False)
+
+        class SampleNetWithExtraParam(nn.Module):
+            def __init__(self):
+                super().__init__()
+
+                self.fc = nn.Linear(8, 16)
+                self.extra_param = nn.Parameter(torch.Tensor(16, 2))
+
+            def forward(self, x):
+                x = self.fc(x)
+                x = x.matmul(self.extra_param)
+                return x
+
+        model = SampleNetWithExtraParam()
+        self.assertFalse(inspector.validate(model))
+
+        model.extra_param.requires_grad = False
+        self.assertTrue(inspector.validate(model))
+
+    def test_unsupported_layer(self):
+        class SampleNetWithTransformer(nn.Module):
+            def __init__(self):
+                super().__init__()
+
+                self.fc = nn.Linear(8, 16)
+                self.encoder = nn.Transformer()
+
+            def forward(self, x):
+                x = self.fc(x)
+                x = self.encoder(x)
+                return x
+
+        model = SampleNetWithTransformer()
+        inspector = dp_inspector.DPModelInspector(should_throw=False)
+        self.assertFalse(inspector.validate(model))
+
+    def test_conv2d(self):
+        inspector = dp_inspector.DPModelInspector(should_throw=False)
+
+        self.assertTrue(
+            inspector.validate(nn.Conv2d(in_channels=3, out_channels=6, kernel_size=1, groups=1))
+        )
+        self.assertTrue(
+            inspector.validate(nn.Conv2d(in_channels=3, out_channels=6, kernel_size=1, groups=3))
+        )
+        self.assertFalse(
+            inspector.validate(nn.Conv2d(in_channels=6, out_channels=6, kernel_size=1, groups=2))
         )
