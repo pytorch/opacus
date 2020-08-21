b64cde484888b4896124a7872a318a5974d20655
John Nguyen <ngjhn@fb.com>
2020-08-20 20:46:15 -0700

    Add docstrings to stats.py
    Summary: Add doc strings and type annotation for `utils/stats.py`
    
    Reviewed By: Darktex
    
    Differential Revision: D22796230
    
    fbshipit-source-id: 5bdfbbc2f456031670b39e4743b51e13dbf885cc

--- torchdp/utils/stats.py
+++ torchdp/utils/stats.py
@@ -1,41 +1,8 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-"""
-This helper file provides simple wrapper around tensorboard for gathering
-statistics as a research tool to get insight into differential privacy and
-to observe how clipping, and noising affects loss, accuracy, ...
-Internal Privacy metrics (`StatType.PRIVACY` and StatType.CLIPPING` are
-already added to the code and need only be activated by adding the stat
-as shown in the example. Other stat types need to be added to the stat
-and updated properly using `update` function. Example below tries to
-show simple use cases.
-
-Example:
-    If an instance of `tensorboard.SummaryWriter` exists it can be used
-    for stat gathering, o.w. one can be created, e.g.
-
-        stats.set_global_summary_writer(tensorboard.SummaryWriter())
-
-    If you like to get stats about clipping you can add the following line
-    to your main file. That is it. The frequency, defines the sampling frequency
-    of the statistics. By default the samples are averaged and the average is
-    reported every `1 / frequency` times.
-
-        stats.add(
-            stats.Stat(stats.StatType.CLIPPING, 'AllLayers', frequency=0.1))
-
-    To add stats about test accuracy you can do:
-
-        stats.add(stats.StatType.TEST, 'accuracy', frequency=0.1))
-
-    and then update the stat in the proper location using:
-
-        # calculate accuracy@1 for example
-        stats.update(stats.StatType.TEST, acc1=acc1_value)
-"""
 
 from enum import IntEnum
-from typing import Any
+from typing import Any, Dict, Optional
 import warnings
 
 
@@ -50,6 +17,15 @@ except ImportError:
 
 
 class StatType(IntEnum):
+    r"""
+    This enum covers all the stat types we currently support.
+
+    1. LOSS: Monitors the training loss.
+    2. CLIPPING: Monitors clipping by logging the norm of the gradients across iterations
+    3. PRIVACY: Logs Epsilon so you can see how it evolves during training
+    4. TRAIN: This is a TB namespace where you can attach training metrics
+    5. TEST: Similar to TRAIN, just another TB namespace to log things under
+    """
     LOSS = 1
     CLIPPING = 2
     PRIVACY = 3
@@ -58,53 +34,98 @@ class StatType(IntEnum):
 
 
 class Stat:
-    """
-    Simple wrapper around tensorboard's SummaryWriter.add_scalar
-    to allow sampling and easier interface.
+    r"""
+    Wrapper around tensorboard's ``SummaryWriter.add_scalar``, allowing for sampling
+    and easier interface.
 
-     Args:
-        stat_type: Type of the statistic from `StatType`.
-        name: Name of the stat that is used to identify this `Stat`
-        for update or to view in tensorboard.
-        frequency: The frequency of stat gathering a value in [0, 1]
-        where e.g. 1 means report to tensorboard any time `log` is
-        called and 0.1 means report only 1 out of 10 times.
-        aggr: The aggregation strategy used for reporting, e.g. if
-        `frequency = 0.1` and `aggr='avg'` then `log` averages 10 samples
-        and the reports to tensorboard this average once every 10 samples.
-        Current valid values are 'avg' and 'sample'.
-        summary_writer:
+    Use this to gather and visualize statistics to get insight about
+    differential privacy parameters, and to observe how clipping and noising affects the training process
+    (loss, accuracy, etc).
 
-    Example:
+    We have already implemented some common ones inside ``torchdp.utils.stat.StatType``.
 
-        stat = Stat(StatType.CLIPPING, 'tmp', frequency=0.1);
-        for i in range(20):
-            stat.log(i)
-        # reports (iter:0, value:0) and (iter:10, value:4.5) to tensorboard
-    """
+    Internal Privacy metrics (such as ``StatType.PRIVACY`` and ``StatType.CLIPPING``)
+    are already added to the code and need only be activated by adding the stat
+    as shown in the example. Other stat types need to be added to the stat
+    and updated properly using ``update`` function.
 
-    # pyre-fixme[8]: Attribute has type `SummaryWriter`; used as `None`.
-    summary_writer: SummaryWriter = None
+    Examples
+    --------
+    To get stats about clipping you can add the following line
+    to your main file. By default the samples are averaged and the average is
+    reported every ``1 / frequency`` times.
+
+        >>> stat = Stat(StatType.CLIPPING, 'sample_stats', frequency=0.1)
+        >>> for i in range(20):
+        >>>    stat.log(i)
+
+    If an instance of ``tensorboard.SummaryWriter`` exists it can be used
+    for stat gathering by passing it like this:
+
+        >>> stats.set_global_summary_writer(tensorboard.SummaryWriter())
+
+    To add stats about test accuracy you can do:
+
+        >>> stats.add(stats.StatType.TEST, 'accuracy', frequency=0.1)
+
+    and then update the stat meter in the proper location using:
+
+        >>> acc1_value = compute_accuracy(x, y)  # you can supply your metrics functions, and Stats later displays them
+        >>> stats.update(stats.StatType.TEST, acc1=acc1_value)  # pass to Stats the result so that the result gets logged
     """
-    The global `SummaryWriter` from tensorboard, if is `None` on construction
-    of the first `Stat` object, a summary_writer will be created per instance.
-    """
+    summary_writer: Optional[SummaryWriter] = None
 
     def __init__(
-        self, stat_type: StatType, name: str, frequency: float = 1.0, aggr: str = "avg"
+        self,
+        stat_type: StatType,
+        name: str,
+        frequency: float = 1.0,
+        reduction: str = "avg",
     ):
-        self.type: StatType = stat_type
-        self.name: str = name
-        self.report: int = int(1 / frequency)
-        self.aggr: str = aggr
-        self.writer: SummaryWriter = Stat.summary_writer if Stat.summary_writer else SummaryWriter()
+        r"""
+        Parameters
+        ----------
+        stat_type:
+            Type of the statistic from ``StatType``.
+        name:
+            Name of the stat that is used to identify this ``Stat``
+            for update or to view in tensorboard.
+        frequency:
+            The frequency of stat gathering. Its value is in [0, 1],
+            where e.g. 1 means report to tensorboard any time ``log`` is
+            called and 0.1 means report only 1 out of 10 times.
+        reduction:
+            The reduction strategy used for reporting, e.g. if
+            ``frequency = 0.1`` and ``reduction='avg'`` then ``log`` averages 10 samples
+            and reports to tensorboard this average once every 10 samples.
+            Current valid values are 'avg' and 'sample'.
+        """
+        self.type = stat_type
+        self.name = name
+        self.report = int(1 / frequency)
+        self.reduction = reduction
+        self.writer = Stat.summary_writer if Stat.summary_writer else SummaryWriter()
+        self.named_values = []
         self.reset()
 
     def reset(self):
+        """
+        Resets the accumulated metrics.
+        """
         self.named_value = {}
-        self.iter: int = 0
+        self.iter = 0
 
-    def log(self, named_value: Any):
+    def log(self, named_value: Dict[str, Any]):
+        r"""
+        Logs a metrics to tensorboard.
+
+        Generally not used directly (use ``update`` instead).
+
+        Parameters
+        ----------
+        named_value
+            A dictionary of metrics to log
+        """
         if self.iter % self.report == 0:
             for k, v in self.named_value.items():
                 self.writer.add_scalar(
@@ -112,11 +133,16 @@ class Stat:
                 )
         self._aggregate(named_value)
 
-    def _aggregate(self, named_value: Any):
-        if self.aggr == "sample":
-            # pyre-fixme[16]: `Stat` has no attribute `named_values`.
+    def _aggregate(self, named_value: Dict[str, Any]):
+        """
+        Aggregates ``named_value`` using this object's ``reduction`` attribute.
+
+        Args:
+            named_value: The value to aggregate
+        """
+        if self.reduction == "sample":
             self.named_values = named_value
-        elif self.aggr == "avg":
+        elif self.reduction == "avg":
             for k, v in named_value.items():
                 self.named_value[k] = (
                     self.named_value[k] + float(v) / self.report
@@ -131,38 +157,60 @@ Stats = []
 
 
 def set_global_summary_writer(summary_writer: SummaryWriter):
+    """
+    Sets this object's TensorBoard SummaryWriter to an externally provided one.
+
+    Useful if you already have one instantiated and you don't want this to
+    create another unnecessarily.
+
+    Args:
+        summary_writer: The externally provided SummaryWriter
+    """
     Stat.summary_writer = summary_writer
 
 
-def add(*args):
-    """
-    Add statistics gathering to the process.
+def add(*args: Stat):
+    r"""
+    Adds statistics gathering to the process.
+
+    Parameters
+    ----------
+    *args:
+        An iterable of statistics to add
     """
     [Stats.append(stat) for stat in args]
 
 
 def clear():
-    """
-    Clear all stats. After calling this gathering
-    all statistics will stop.
+    r"""
+    Clears all stats and stops collecting statistics.
     """
     Stats.clear()
 
 
 def remove(name: str):
-    """
-    Will remove the stat object nemaed
-    `name` from the global statistics gathering.
+    r"""
+    Removes the Stat of name ``name`` from the global statistics gathering.
+
+    Parameters
+    ----------
+    name:
+        The name of stats to remove
     """
     global Stats
     Stats = [stat for stat in Stats if stat.name != name]
 
 
-# pyre-fixme[9]: stat_type has type `StatType`; used as `None`.
-# pyre-fixme[9]: name has type `str`; used as `None`.
-def reset(stat_type: StatType = None, name: str = None):
-    """
+def reset(stat_type: Optional[StatType] = None, name: Optional[str] = None):
+    r"""
     Resets the stat with given `name` and `stat_type`
+
+    Parameters
+    ----------
+    stat_type:
+        The stat_type to reset
+    name:
+        The name of stats to reset
     """
     [
         stat.reset()
@@ -172,18 +220,24 @@ def reset(stat_type: StatType = None, name: str = None):
     ]
 
 
-# pyre-fixme[9]: stat_type has type `StatType`; used as `None`.
-# pyre-fixme[9]: name has type `str`; used as `None`.
-def update(stat_type: StatType = None, name: str = None, **named_values):
-    """
-    updates the stat(s) with the given `name` and `stat_type`
+def update(
+    stat_type: Optional[StatType] = None,
+    name: Optional[str] = None,
+    **named_values: str,
+):
+    r"""
+    Updates the stat(s) with the given ``name`` and ``stat_type``
 
-    Args:
-        stat_type: type of the stat from `StatType`, could be `None`
-            if `name` is unique.
-        name: name of the stat, could be `None` if there is only one
-            stat for the `stat_type`
-        **named_values: set of values with their names
+    Parameters
+    ----------
+        stat_type:
+            The type of the stat from ``StatType``. Could be ``None``
+            if ``name`` is unique.
+        name:
+            The name of the stat. Could be ``None`` if there is only one
+            stat for the ``stat_type``
+        **named_values:
+            A set of values with their names
     """
     [
         stat.log(named_values)
