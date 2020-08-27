96dc4609056cafe38a423843a582e4dd9b054100
Davide Testuggine <davidet@fb.com>
2020-08-26 17:40:27 -0700

    Restructure CircleCI to be more readable, add GPU testing (#34)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytorch-dp/pull/34
    
    Restructure CircleCI to be more readable, add GPU testing
    
    Differential Revision: D23331768
    
    fbshipit-source-id: ddfd2edd39cc6db23de0181ecc0dafa4f6b682a8

--- setup.py
+++ setup.py
@@ -2,9 +2,35 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
 import os
+import sys
 
-import setuptools
+from setuptools import find_packages, setup
 
+
+REQUIRED_MAJOR = 3
+REQUIRED_MINOR = 7
+
+# Check for python version
+if sys.version_info < (REQUIRED_MAJOR, REQUIRED_MINOR):
+    error = (
+        "Your version of python ({major}.{minor}) is too old. You need "
+        "python >= {required_major}.{required_minor}."
+    ).format(
+        major=sys.version_info.major,
+        minor=sys.version_info.minor,
+        required_minor=REQUIRED_MINOR,
+        required_major=REQUIRED_MAJOR,
+    )
+    sys.exit(error)
+
+DEV_REQUIRES = [
+    "black",
+    "flake8",
+    "sphinx",
+    "sphinx-autodoc-typehints",
+    "mypy>=0.760",
+    "isort",
+]
 
 src_dir = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,7 +42,7 @@ with open("requirements.txt", encoding="utf8") as f:
     required = f.read().splitlines()
 
 
-setuptools.setup(
+setup(
     name="pytorch-dp",
     version="0.1-beta.1",
     author="PyTorch Team",
@@ -26,10 +52,25 @@ setuptools.setup(
     url="https://github.com/facebookresearch/pytorch-dp",
     license="Apache-2.0",
     install_requires=required,
-    packages=setuptools.find_packages(),
+    extras_require={"dev": DEV_REQUIRES},
+    packages=find_packages(),
+    keywords=[
+        "PyTorch",
+        "Differential Privacy",
+        "DP-SGD",
+        "DP SGD",
+        "Privacy Preserving Machine Learning",
+        "PPML",
+        "PPAI",
+    ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License 2.0 (Apache-2.0)",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering",
     ],
     python_requires=">=3.6",
 )
