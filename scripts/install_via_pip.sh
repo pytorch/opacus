a9327969dbdc481c91614cfda65013edef27ab4a
Davide Testuggine <davidet@fb.com>
2020-08-26 19:13:37 -0700

    Restructure CircleCI to be more readable, add GPU testing (#34)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytorch-dp/pull/34
    
    Restructure CircleCI to be more readable, add GPU testing
    
    Reviewed By: ashkan-software
    
    Differential Revision: D23331768
    
    fbshipit-source-id: 54a766cdda2ce7eeb69f249733b4ed72703dbbd7

---
+++ scripts/install_via_pip.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -e
+
+PYTORCH_NIGHTLY=false
+DEPLOY=false
+CHOSEN_TORCH_VERSION=-1
+
+while getopts 'ndv:' flag; do
+  case "${flag}" in
+    n) PYTORCH_NIGHTLY=true ;;
+    d) DEPLOY=true ;;
+    v) CHOSEN_TORCH_VERSION=${OPTARG};;
+    *) echo "usage: $0 [-n] [-d] [-v version]" >&2
+       exit 1 ;;
+    esac
+  done
+
+# NOTE: Only Debian variants are supported, since this script is only
+# used by our tests on CircleCI.
+
+# install nodejs and yarn for website build
+sudo apt install apt-transport-https ca-certificates
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt install nodejs
+sudo apt install yarn
+
+# yarn needs terminal info
+export TERM=xterm
+
+# NOTE: All of the below installs use sudo, b/c otherwise pip will get
+# permission errors installing in the docker container. An alternative would be
+# to use a virtualenv, but that would lead to bifurcation of the CircleCI config
+# since we'd need to source the environemnt in each step.
+
+# upgrade pip
+sudo pip install --upgrade pip
+
+# install with dev dependencies
+sudo pip install -e .[dev]
+
+# install pytorch nightly if asked for
+if [[ $PYTORCH_NIGHTLY == true ]]; then
+  sudo pip install --upgrade --pre torch -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
+else
+  # If no version specified, upgrade to latest release.
+  if [[ $CHOSEN_TORCH_VERSION == -1 ]]; then
+    sudo pip install --upgrade torch
+  else
+    sudo pip install torch==$CHOSEN_TORCH_VERSION
+  fi
+fi
+
+# install deployment bits if asked for
+if [[ $DEPLOY == true ]]; then
+  sudo pip install beautifulsoup4 ipython nbconvert
+fi
