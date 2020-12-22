a33acd927d880dc30fbab5eabe8d6bd330134e60
Sayan Ghosh <sayanghosh@fb.com>
2020-12-21 19:33:55 -0800

    Adjust tolerance for conv3d to avoid flakiness
    Summary:
    Adjust tolerance for conv3d thresholds
    Conv 3D tests are flaky not because of some underlying problem, but due to tolerance issues
    So adjusting the tolerance accordingly.
    
    Reviewed By: Darktex
    
    Differential Revision: D25670974
    
    fbshipit-source-id: 772e84eb785d78bf0167cf18e99d776b7f3a4aa6

--- opacus/tests/grad_samples/conv3d.py
+++ opacus/tests/grad_samples/conv3d.py
@@ -12,127 +12,127 @@ class Conv3d_test(GradSampleHooks_test):
         N, C, D, H, W = 32, 1, 10, 10, 10
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, C, kernel_size=2)
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_landscape_image_kernel2(self):
         N, C, D, H, W = 32, 3, 4, 9, 16
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=2)
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_portrait_image_kernel2(self):
         N, C, D, H, W = 32, 3, 4, 16, 9
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=2)
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_square_image_kernel3(self):
         N, C, D, H, W = 32, 3, 10, 10, 10
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=3)
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_landscape_image_kernel3(self):
         N, C, D, H, W = 32, 3, 5, 9, 16
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=3)
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_portrait_image_kernel3(self):
         N, C, D, H, W = 32, 3, 5, 16, 9
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=3)
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_square_image_stride_2(self):
         N, C, D, H, W = 32, 3, 10, 10, 10
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=2, stride=2)
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_landscape_image_stride_2(self):
         N, C, D, H, W = 32, 3, 5, 9, 16
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=2, stride=2)
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_portrait_image_stride_2(self):
         N, C, D, H, W = 32, 3, 5, 16, 9
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=2, stride=2)
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_square_image_pad_2(self):
         N, C, D, H, W = 32, 3, 10, 10, 10
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=2, padding=2)
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_landscape_image_pad_2(self):
         N, C, D, H, W = 32, 3, 5, 9, 16
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=2, padding=2)
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_portrait_image_pad_2(self):
         N, C, D, H, W = 32, 3, 5, 16, 9
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=2, padding=2)
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_square_image_variable_pad(self):
         N, C, D, H, W = 32, 3, 10, 10, 10
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=2, padding=(1, 2, 3))
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_landscape_image_variable_pad(self):
         N, C, D, H, W = 32, 3, 5, 9, 16
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=2, padding=(1, 2, 3))
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_portrait_image_variable_pad(self):
         N, C, D, H, W = 32, 3, 5, 16, 9
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=2, padding=(1, 2, 3))
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_square_image_variable_kernel(self):
         N, C, D, H, W = 32, 3, 10, 10, 10
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=(1, 2, 3))
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_landscape_image_variable_kernel(self):
         N, C, D, H, W = 32, 3, 4, 9, 16
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=(1, 2, 3))
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_portrait_image_variable_kernel(self):
         N, C, D, H, W = 32, 3, 4, 16, 9
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=(1, 2, 3))
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_square_image_variable_stride(self):
         N, C, D, H, W = 32, 3, 10, 10, 10
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=2, stride=(1, 2, 3))
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_landscape_image_variable_stride(self):
         N, C, D, H, W = 32, 3, 5, 9, 16
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=2, stride=(1, 2, 3))
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     def test_portrait_image_variable_stride(self):
         N, C, D, H, W = 32, 3, 5, 16, 9
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, 2 * C, kernel_size=2, stride=(1, 2, 3))
-        self.run_test(x, conv, batch_first=True, atol=10e-8, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-6, rtol=10e-3)
 
     """
     These generally show up in upper layers. At this point, C is just generic "channels"
@@ -143,34 +143,34 @@ class Conv3d_test(GradSampleHooks_test):
         N, C, D, H, W = 4, 16, 12, 12, 12
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, C * 2, kernel_size=3, stride=1, groups=2)
-        self.run_test(x, conv, batch_first=True, atol=10e-5, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-5, rtol=10e-3)
 
     def test_4d_input_2_groups_shrinking(self):
         N, C, D, H, W = 4, 32, 12, 12, 12
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, C // 2, kernel_size=3, stride=1, groups=2)
-        self.run_test(x, conv, batch_first=True, atol=10e-5, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-5, rtol=10e-3)
 
     def test_4d_input_4_groups_expanding(self):
         N, C, D, H, W = 4, 16, 12, 12, 12
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, C * 2, kernel_size=3, stride=1, groups=4)
-        self.run_test(x, conv, batch_first=True, atol=10e-5, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-5, rtol=10e-3)
 
     def test_4d_input_4_groups_shrinking(self):
         N, C, D, H, W = 4, 32, 12, 12, 12
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, C // 2, kernel_size=3, stride=1, groups=4)
-        self.run_test(x, conv, batch_first=True, atol=10e-5, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-5, rtol=10e-3)
 
     def test_4d_input_16_groups_expanding(self):
         N, C, D, H, W = 4, 16, 12, 12, 12
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, C * 2, kernel_size=3, stride=1, groups=16)
-        self.run_test(x, conv, batch_first=True, atol=10e-5, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-5, rtol=10e-3)
 
     def test_4d_input_16_groups_shrinking(self):
         N, C, D, H, W = 4, 32, 12, 12, 12
         x = torch.randn([N, C, D, H, W])
         conv = nn.Conv3d(C, C // 2, kernel_size=3, stride=1, groups=16)
-        self.run_test(x, conv, batch_first=True, atol=10e-5, rtol=10e-4)
+        self.run_test(x, conv, batch_first=True, atol=10e-5, rtol=10e-3)
