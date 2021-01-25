dbe2428813d5f014476018ea2ecfc5528671324f
Davide Testuggine <davidet@fb.com>
2021-01-25 15:37:35 -0800

    Migrate tests for DP Layers to Hypothesis (#120)
    Summary:
    Pull Request resolved: https://github.com/pytorch/opacus/pull/120
    
    See Issue 119 for context: https://github.com/pytorch/opacus/issues/119
    
    Note: this PR introduces a failing test. This is by design, as it outlines an issue with our current code. Issue https://github.com/pytorch/opacus/issues/123 will fix it and can be tested by this test, which must turn green
    
    Reviewed By: ashkan-software
    
    Differential Revision: D25318869
    
    fbshipit-source-id: c955cb5249924f1fceec55c01d70476cea163e70

---
+++ opacus/tests/dp_layers/dp_multihead_attention_test.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+from typing import Optional
+
+import hypothesis.strategies as st
+import torch
+import torch.nn as nn
+from hypothesis import given, settings
+from opacus.layers import DPMultiheadAttention
+
+from .common import DPModules_test
+
+
+def attn_train_fn(
+    model: nn.Module,
+    *args,
+    **kwargs,
+):
+    model.train()
+    criterion = nn.MSELoss()
+    logits, attn_weights = model(*args, **kwargs)
+    y = torch.zeros_like(logits)
+    loss = criterion(logits, y)
+    loss.backward()
+
+
+class DPMultiheadAttention_test(DPModules_test):
+    @given(
+        batch_size=st.integers(1, 5),
+        src_seq_len=st.integers(1, 6),
+        tgt_seq_len=st.integers(1, 6),
+        num_heads=st.integers(1, 3),
+        bias=st.booleans(),
+        add_bias_kv=st.booleans(),
+        add_zero_attn=st.booleans(),
+        kdim=st.integers(2, 8) | st.none(),
+        vdim=st.integers(2, 8) | st.none(),
+    )
+    @settings(deadline=10000)
+    def test_attn(
+        self,
+        batch_size: int,
+        src_seq_len: int,
+        tgt_seq_len: int,
+        num_heads: int,
+        bias: bool,
+        add_bias_kv: bool,
+        add_zero_attn: bool,
+        kdim: Optional[int],
+        vdim: Optional[int],
+    ):
+        embed_dim = 4 * num_heads  # embed_dim must be divisible by num_heads
+
+        attn = nn.MultiheadAttention(
+            embed_dim,
+            num_heads,
+            dropout=0.0,  # Untestable between two different implementations
+            bias=bias,
+            add_bias_kv=add_bias_kv,
+            add_zero_attn=add_zero_attn,
+            kdim=kdim,
+            vdim=vdim,
+        )
+        dp_attn = DPMultiheadAttention(
+            embed_dim,
+            num_heads,
+            dropout=0.0,  # Untestable between two different implementations
+            bias=bias,
+            add_bias_kv=add_bias_kv,
+            add_zero_attn=add_zero_attn,
+            kdim=kdim,
+            vdim=vdim,
+        )
+
+        dp_attn.load_state_dict(attn.state_dict())
+
+        q = torch.randn(tgt_seq_len, batch_size, embed_dim)
+        k = torch.randn(
+            src_seq_len, batch_size, kdim if kdim is not None else embed_dim
+        )
+        v = torch.randn(
+            src_seq_len, batch_size, vdim if vdim is not None else embed_dim
+        )
+
+        self.compare_forward_outputs(
+            attn,
+            dp_attn,
+            q,
+            k,
+            v,
+            output_names=("attn_out", "attn_out_weights"),
+            atol=1e-5,
+            rtol=1e-3,
+            key_padding_mask=None,
+            need_weights=True,
+            attn_mask=None,
+        )
+
+        self.compare_gradients(
+            attn,
+            dp_attn,
+            attn_train_fn,
+            q,
+            k,
+            v,
+            atol=1e-5,
+            rtol=1e-3,
+            key_padding_mask=None,
+            need_weights=True,
+            attn_mask=None,
+        )
