9c5603fece07923b510d9523a66e7809b27ea8fa
Peter Romov <romovpa@gmail.com>
2021-10-12 01:17:34 +0100

    Updated dp_layers/dp_rnn_test.py (former dp_lstm_test.py)

--- opacus/tests/dp_layers/dp_rnn_test.py
+++ opacus/tests/dp_layers/dp_rnn_test.py
@@ -3,25 +3,26 @@
 
 from typing import Optional, Tuple, Union
 
+from hypothesis import given, settings
 import hypothesis.strategies as st
 import torch
 import torch.nn as nn
-from hypothesis import given, settings
+from torch.nn.utils.rnn import PackedSequence
+
 from opacus.layers import DPLSTM, DPRNN, DPGRU
 from opacus.utils.packed_sequences import _gen_packed_data
-from torch.nn.utils.rnn import PackedSequence
 
 from .common import DPModules_test
 
 
-def lstm_train_fn(
-    model: nn.Module,
-    x: Union[torch.Tensor, PackedSequence],
-    state_init: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
+def rnn_train_fn(
+        model: nn.Module,
+        x: Union[torch.Tensor, PackedSequence],
+        state_init: Optional[Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]] = None,
 ):
     model.train()
     criterion = nn.MSELoss()
-    logits, (hn, cn) = model(x, state_init)
+    logits, _ = model(x, state_init)
     if isinstance(logits, PackedSequence):
         y = torch.zeros_like(logits[0])
         loss = criterion(logits[0], y)
@@ -33,6 +34,7 @@ def lstm_train_fn(
 
 class DPLSTM_test(DPModules_test):
     @given(
+        mode=st.one_of(st.just('rnn'), st.just('gru'), st.just('lstm')),
         batch_size=st.integers(1, 5),
         seq_len=st.integers(1, 6),
         emb_size=st.integers(5, 10),
@@ -43,132 +45,10 @@ class DPLSTM_test(DPModules_test):
         batch_first=st.booleans(),
         zero_init=st.booleans(),
         packed_input_flag=st.integers(
-            0, 2
-        ),  # 0 indicates no packed sequence input, 1 indicates packed sequence input in sorted order, 2 indicates packed sequence input in unsorted order
-    )
-    @settings(deadline=20000)
-    def test_lstm(
-        self,
-        batch_size: int,
-        seq_len: int,
-        emb_size: int,
-        hidden_size: int,
-        num_layers: int,
-        bidirectional: bool,
-        bias: bool,
-        batch_first: bool,
-        zero_init: bool,
-        packed_input_flag: int,
-    ):
-        lstm = nn.LSTM(
-            emb_size,
-            hidden_size,
-            num_layers=num_layers,
-            batch_first=batch_first,
-            bidirectional=bidirectional,
-            bias=bias,
-        )
-        dp_lstm = DPLSTM(
-            emb_size,
-            hidden_size,
-            num_layers=num_layers,
-            batch_first=batch_first,
-            bidirectional=bidirectional,
-            bias=bias,
-        )
-
-        dp_lstm.load_state_dict(lstm.state_dict())
-
-        if packed_input_flag == 0:
-            x = (
-                torch.randn([batch_size, seq_len, emb_size])
-                if batch_first
-                else torch.randn([seq_len, batch_size, emb_size])
-            )
-        elif packed_input_flag == 1:
-            x = _gen_packed_data(
-                batch_size, seq_len, emb_size, batch_first, sorted_=True
-            )
-        elif packed_input_flag == 2:
-            x = _gen_packed_data(
-                batch_size, seq_len, emb_size, batch_first, sorted_=False
-            )
-
-        if zero_init:
-            self.compare_forward_outputs(
-                lstm,
-                dp_lstm,
-                x,
-                output_names=("out", "hn", "cn"),
-                atol=1e-5,
-                rtol=1e-3,
-            )
-
-            self.compare_gradients(
-                lstm,
-                dp_lstm,
-                lstm_train_fn,
-                x,
-                atol=1e-5,
-                rtol=1e-3,
-            )
-
-        else:
-            num_directions = 2 if bidirectional else 1
-            h0 = torch.randn([num_layers * num_directions, batch_size, hidden_size])
-            c0 = torch.randn([num_layers * num_directions, batch_size, hidden_size])
-            self.compare_forward_outputs(
-                lstm,
-                dp_lstm,
-                x,
-                (h0, c0),
-                output_names=("out", "hn", "cn"),
-                atol=1e-5,
-                rtol=1e-3,
-            )
-            self.compare_gradients(
-                lstm,
-                dp_lstm,
-                lstm_train_fn,
-                x,
-                (h0, c0),
-                atol=1e-5,
-                rtol=1e-3,
-            )
-
-
-def rnn_train_fn(
-    model: nn.Module,
-    x: Union[torch.Tensor, PackedSequence],
-    state_init: Optional[torch.Tensor] = None,
-):
-    model.train()
-    criterion = nn.MSELoss()
-    logits, hn = model(x, state_init)
-    if isinstance(logits, PackedSequence):
-        y = torch.zeros_like(logits[0])
-        loss = criterion(logits[0], y)
-    else:
-        y = torch.zeros_like(logits)
-        loss = criterion(logits, y)
-    loss.backward()
-
-
-class DPRNN_test(DPModules_test):
-    @given(
-        mode=st.one_of(st.just('rnn'), st.just('gru')),
-        batch_size=st.integers(1, 5),
-        seq_len=st.integers(1, 6),
-        emb_size=st.integers(5, 10),
-        hidden_size=st.integers(3, 7),
-        num_layers=st.integers(1, 3),
-        bidirectional=st.booleans(),
-        bias=st.booleans(),
-        batch_first=st.booleans(),
-        zero_init=st.booleans(),
-        packed_input_flag=st.integers(
-            0, 2
-        ),  # 0 indicates no packed sequence input, 1 indicates packed sequence input in sorted order, 2 indicates packed sequence input in unsorted order
+            0,  # no packed sequence input
+            #1,  # packed sequence input in sorted order
+            2,  # packed sequence input in unsorted order
+        ),
     )
     @settings(deadline=20000)
     def test_rnn(
@@ -185,14 +65,19 @@ class DPRNN_test(DPModules_test):
         zero_init: bool,
         packed_input_flag: int,
     ):
+        use_cn = False
         if mode == 'rnn':
             original_rnn_class = nn.RNN
             dp_rnn_class = DPRNN
         elif mode == 'gru':
             original_rnn_class = nn.GRU
             dp_rnn_class = DPGRU
+        elif mode == 'lstm':
+            original_rnn_class = nn.LSTM
+            dp_rnn_class = DPLSTM
+            use_cn = True
         else:
-            raise RuntimeError('Invalid RNN mode')
+            raise ValueError('Invalid RNN mode')
 
         rnn = original_rnn_class(
             emb_size,
@@ -227,13 +112,15 @@ class DPRNN_test(DPModules_test):
             x = _gen_packed_data(
                 batch_size, seq_len, emb_size, batch_first, sorted_=False
             )
+        else:
+            raise ValueError('Invalid packed input flag')
 
         if zero_init:
             self.compare_forward_outputs(
                 rnn,
                 dp_rnn,
                 x,
-                output_names=("out", "hn"),
+                output_names=("out", "hn", "cn") if use_cn else ("out", "hn"),
                 atol=1e-5,
                 rtol=1e-3,
             )
@@ -250,12 +137,13 @@ class DPRNN_test(DPModules_test):
         else:
             num_directions = 2 if bidirectional else 1
             h0 = torch.randn([num_layers * num_directions, batch_size, hidden_size])
+            c0 = torch.randn([num_layers * num_directions, batch_size, hidden_size])
             self.compare_forward_outputs(
                 rnn,
                 dp_rnn,
                 x,
-                h0,
-                output_names=("out", "hn"),
+                (h0, c0) if use_cn else h0,
+                output_names=("out", "hn", "cn") if use_cn else ("out", "hn"),
                 atol=1e-5,
                 rtol=1e-3,
             )
@@ -264,7 +152,7 @@ class DPRNN_test(DPModules_test):
                 dp_rnn,
                 rnn_train_fn,
                 x,
-                h0,
+                (h0, c0) if use_cn else h0,
                 atol=1e-5,
                 rtol=1e-3,
             )
