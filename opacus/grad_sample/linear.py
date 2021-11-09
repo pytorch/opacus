97d83694f210a225b95b14e10f230d9479a22119
Pierre Stock <pstock@fb.com>
2021-11-09 13:59:51 +0100

    update requirements and revert grad_sample/linear

--- opacus/grad_sample/linear.py
+++ opacus/grad_sample/linear.py
@@ -1,33 +1,31 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
-from typing import Dict
-
 import torch
 import torch.nn as nn
 
-from .utils import register_grad_sampler
+from .utils import create_or_extend_grad_sample, register_grad_sampler
 
 
 @register_grad_sampler(nn.Linear)
 def compute_linear_grad_sample(
-    layer: nn.Linear, activations: torch.Tensor, backprops: torch.Tensor
-) -> Dict[nn.Parameter, torch.Tensor]:
+    layer: nn.Linear, A: torch.Tensor, B: torch.Tensor, batch_dim: int = 0
+) -> None:
     """
     Computes per sample gradients for ``nn.Linear`` layer
 
     Args:
         layer: Layer
-        activations: Activations
-        backprops: Backpropagations
+        A: Activations
+        B: Backpropagations
+        batch_dim: Batch dimension position
     """
-    # With activations with 3 dimensions (transformers) the original formula does not aggregate correctly
-    if activations.ndim == 2:
-        gs = torch.einsum("n...i,n...j->nij", backprops, activations)
-    else:
-        gs =  torch.einsum("nki,nkj->nij", backprops, activations)
-    ret = {layer.weight: gs}
+    gs = torch.einsum("n...i,n...j->nij", B, A)
+    create_or_extend_grad_sample(layer.weight, gs, batch_dim)
     if layer.bias is not None:
-        ret[layer.bias] = torch.einsum("n...k->nk", backprops)
 
-    return ret
+        create_or_extend_grad_sample(
+            layer.bias,
+            torch.einsum("n...k->nk", B),
+            batch_dim,
+        )
