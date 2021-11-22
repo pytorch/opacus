1df13884a49f3318f4500b76ae5410ea3f778a61
Alexandre Sablayrolles <asablayrolles@fb.com>
2021-11-22 23:57:41 +0100

    Fix lint

--- opacus/tests/dpdataloader_test.py
+++ opacus/tests/dpdataloader_test.py
@@ -1,13 +1,8 @@
 import unittest
-from typing import Optional
 
 import torch
-import torch.nn as nn
-from opacus import PrivacyEngine
-from opacus.data_loader import DPDataLoader, switch_generator
-from opacus.grad_sample import GradSampleModule
-from opacus.optimizers import DPOptimizer
-from torch.utils.data import DataLoader, TensorDataset, Sampler
+from opacus.data_loader import DPDataLoader
+from torch.utils.data import TensorDataset
 
 
 class DPDataLoaderTest(unittest.TestCase):
@@ -19,7 +14,7 @@ class DPDataLoaderTest(unittest.TestCase):
     def test_collate_classes(self):
         x = torch.randn(self.data_size, self.dimension)
         y = torch.randint(low=0, high=self.num_classes, size=(self.data_size,))
-        
+
         dataset = TensorDataset(x, y)
         data_loader = DPDataLoader(dataset, sample_rate=1e-5)
 
@@ -27,13 +22,12 @@ class DPDataLoaderTest(unittest.TestCase):
         self.assertEqual(x_b.size(0), 0)
         self.assertEqual(y_b.size(0), 0)
 
-
     def test_collate_tensor(self):
         x = torch.randn(self.data_size, self.dimension)
-        
+
         dataset = TensorDataset(x)
         data_loader = DPDataLoader(dataset, sample_rate=1e-5)
 
-        s,  = next(iter(data_loader))
+        (s,) = next(iter(data_loader))
 
         self.assertEqual(s.size(0), 0)
