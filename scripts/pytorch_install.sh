90259c7e8233f612b580ccc973c74cdcd127c7f0
Igor Shilov <shilov@fb.com>
2021-11-25 17:59:22 +0000

    update python/torch versions

--- scripts/pytorch_install.sh
+++ scripts/pytorch_install.sh
@@ -5,41 +5,28 @@
 set -e
 TORCH_VERSION=$1
 
-if [ "$TORCH_VERSION" = "1.4.0" ]
+if [ "$TORCH_VERSION" = "1.8.0" ]
 then
-    TORCHVISION_VERSION="0.5.0"
-elif [ "$TORCH_VERSION" = "1.5.0" ]
+    TORCHVISION_VERSION="0.9.0"
+    TORCHCSPRNG_VERSION="0.2.0"
+elif [ "$TORCH_VERSION" = "1.8.1" ]
 then
-    TORCHVISION_VERSION="0.6.0"
-elif [ "$TORCH_VERSION" = "1.5.1" ]
+    TORCHVISION_VERSION="0.9.1"
+elif [ "$TORCH_VERSION" = "1.9.0" ]
 then
-    TORCHVISION_VERSION="0.6.1"
-elif [ "$TORCH_VERSION" = "1.6.0" ]
+    TORCHVISION_VERSION="0.10.0"
+elif [ "$TORCH_VERSION" = "1.9.1" ]
 then
-    TORCHVISION_VERSION="0.7"
-    TORCHCSPRNG_VERSION="0.1.2"
-elif [ "$TORCH_VERSION" = "1.7.0" ]
-then
-    TORCHVISION_VERSION="0.8.1"
-    TORCHCSPRNG_VERSION="0.1.3"
-elif [ "$TORCH_VERSION" = "1.7.1" ]
-then
-    TORCHVISION_VERSION="0.8.2"
-    TORCHCSPRNG_VERSION="0.1.4"
+    TORCHVISION_VERSION="0.10.1"
 fi
+
 pip install torch=="${TORCH_VERSION}"
 pip install torchvision==${TORCHVISION_VERSION}
 
 # torchcsprng
-if [ "$TORCH_VERSION" = "1.4.0" ]
+if [ "$TORCH_VERSION" = "1.8.0" ]
 then
-    echo "No torchcsprng"
-elif [ "$TORCH_VERSION" = "1.5.0" ]
-then
-    echo "No torchcsprng"
-elif [ "$TORCH_VERSION" = "1.5.1" ]
-then
-    echo "No torchcsprng"
-else
     pip install torchcsprng==${TORCHCSPRNG_VERSION}
+else
+    echo "No torchcsprng"
 fi
