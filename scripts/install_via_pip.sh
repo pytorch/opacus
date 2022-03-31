cfeb5858e01eca5b67f33542348bfa92b60deca5
Karthik Prasad <krp@fb.com>
2022-03-31 11:27:39 -0700

    Migrate from ubuntu 16.04 to 20.04 on CircleCI (#403)
    Summary:
    ## Types of changes
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to change)
    - [ ] Docs change / refactoring / dependency upgrade
    
    ## Motivation and Context / Related issue
    Closes https://github.com/pytorch/opacus/issues/398
    Reference: https://circleci.com/docs/2.0/configuration-reference/
    
    ## How Has This Been Tested (if it applies)
    
    Circle CI test
    
    ## Checklist
    - [x] The documentation is up-to-date with the changes I made.
    - [x] I have read the **CONTRIBUTING** document and completed the CLA (see **CONTRIBUTING**).
    - [x] All tests passed, and additional code has been covered with new tests.
    
    Pull Request resolved: https://github.com/pytorch/opacus/pull/403
    
    Reviewed By: ffuuugor
    
    Differential Revision: D35270107
    
    Pulled By: karthikprasad
    
    fbshipit-source-id: 3ebf98904125a0518a37de0ec68feaaafa167ea4

--- scripts/install_via_pip.sh
+++ scripts/install_via_pip.sh
@@ -33,11 +33,6 @@ while getopts 'ncdv:' flag; do
 # NOTE: Only Debian variants are supported, since this script is only
 # used by our tests on CircleCI.
 
-
-sudo apt-get update
-sudo apt-get upgrade ca-certificates -y
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
