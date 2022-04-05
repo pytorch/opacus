455834eeed2f998389624f1ca42d7fa5b1483053
Peter Romov <romovpa@fb.com>
2022-04-05 15:21:49 +0100

    black

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ with open("dev_requirements.txt", encoding="utf8") as f:
     dev_required = [
         # This is to process correctly git repositories as dependencies
         # https://stackoverflow.com/questions/32688688/how-to-write-setup-py-to-include-a-git-repository-as-a-dependency
-        re.sub(r'-e (git\+.*egg=(.*))', r'\2 @ \1', line)
+        re.sub(r"-e (git\+.*egg=(.*))", r"\2 @ \1", line)
         for line in f.read().splitlines()
     ]
 
