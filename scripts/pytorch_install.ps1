2a52f0b60ff02f63bcca73f0b51e792be1688ee5
Peter Romov <romovpa@fb.com>
2022-05-31 15:16:47 +0100

    Test Opacus with the latest PyTorch version (#434)
    * Removed flag forcing pytorch-1.8 install
    * Removed torchcsprng installation
    * Ignore tests depending on csprng

--- scripts/pytorch_install.ps1
+++ scripts/pytorch_install.ps1
@@ -16,7 +16,6 @@
 [string]$TORCH_VERSION=$args[0]
 If ($TORCH_VERSION -eq "1.8.0") {
   $TORCHVISION_VERSION="0.9.0"
-  $TORCHCSPRNG_VERSION="0.2.0"
 } Elseif ( $TORCH_VERSION -eq "1.8.1" ) {
   $TORCHVISION_VERSION="0.9.1"
 } Elseif ( $TORCH_VERSION -eq "1.9.0" ) {
@@ -26,8 +25,3 @@ If ($TORCH_VERSION -eq "1.8.0") {
 }
 pip install torch==$TORCH_VERSION+cpu torchvision==$TORCHVISION_VERSION+cpu -f https://download.pytorch.org/whl/torch_stable.html
 
-If ($TORCH_VERSION -eq "1.8.0") {
-  pip install torchcsprng==$TORCHCSPRNG_VERSION+cpu -f https://download.pytorch.org/whl/torch_stable.html
-} Else {
-  echo "No torchcsprng"
-}
