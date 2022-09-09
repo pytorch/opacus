d968a6d8ae10f9cc028da1f05067599b2048a5ba
Igor Shilov <shilov@fb.com>
2022-09-09 03:45:41 -0700

    disable padding=same for experimental hook (#498)
    Summary:
    The tests are failing because our experimental `convolution as a backward` GSM can't handle `padding="same"`
    
    Since this grad sampler is not used by default and mostly exists for aesthetic reasons, I think disabling the test is the best course of action.
    
    Pull Request resolved: https://github.com/pytorch/opacus/pull/498
    
    Reviewed By: karthikprasad
    
    Differential Revision: D39357867
    
    Pulled By: alexandresablayrolles
    
    fbshipit-source-id: 89209aef6b61d026db724df61fce4997c44a3f9f

--- opacus/tests/grad_samples/conv2d_test.py
+++ opacus/tests/grad_samples/conv2d_test.py
@@ -85,20 +85,23 @@ class Conv2d_test(GradSampleHooks_test):
             rtol=10e-4,
             ew_compatible=is_ew_compatible,
         )
-        # Test 'convolution as a backward' GSM
-        conv2d_gsm = GradSampleModule.GRAD_SAMPLERS[nn.Conv2d]
-        GradSampleModule.GRAD_SAMPLERS[
-            nn.Conv2d
-        ] = convolution2d_backward_as_a_convolution
-        self.run_test(
-            x,
-            conv,
-            batch_first=True,
-            atol=10e-5,
-            rtol=10e-4,
-            ew_compatible=is_ew_compatible,
-        )
-        GradSampleModule.GRAD_SAMPLERS[nn.Conv2d] = conv2d_gsm
+
+        if padding != "same":
+            # Test 'convolution as a backward' GSM
+            # 'convolution as a backward' doesn't support padding=same
+            conv2d_gsm = GradSampleModule.GRAD_SAMPLERS[nn.Conv2d]
+            GradSampleModule.GRAD_SAMPLERS[
+                nn.Conv2d
+            ] = convolution2d_backward_as_a_convolution
+            self.run_test(
+                x,
+                conv,
+                batch_first=True,
+                atol=10e-5,
+                rtol=10e-4,
+                ew_compatible=is_ew_compatible,
+            )
+            GradSampleModule.GRAD_SAMPLERS[nn.Conv2d] = conv2d_gsm
 
     @given(
         B=st.integers(1, 4),
