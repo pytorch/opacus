c23a0b5dd1f0b2557e6576637623b33b3acd8d8e
Igor Shilov <shilov@fb.com>
2023-01-14 00:27:49 +0000

    version bump

--- scripts/pytorch_install.sh
+++ scripts/pytorch_install.sh
@@ -16,18 +16,9 @@
 set -e
 TORCH_VERSION=$1
 
-if [ "$TORCH_VERSION" = "1.8.0" ]
+if [ "$TORCH_VERSION" = "1.13.1" ]
 then
-    TORCHVISION_VERSION="0.9.0"
-elif [ "$TORCH_VERSION" = "1.8.1" ]
-then
-    TORCHVISION_VERSION="0.9.1"
-elif [ "$TORCH_VERSION" = "1.9.0" ]
-then
-    TORCHVISION_VERSION="0.10.0"
-elif [ "$TORCH_VERSION" = "1.9.1" ]
-then
-    TORCHVISION_VERSION="0.10.1"
+    TORCHVISION_VERSION="0.14.1"
 fi
 
 pip install torch=="${TORCH_VERSION}"
