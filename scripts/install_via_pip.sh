120976510396d16702c992997bb6d76559fbf3b0
Huanyu Zhang <huanyuzhang@meta.com>
2024-09-05 12:13:06 -0700

    Fix github action CI for GPU (#625)
    Summary: Pull Request resolved: https://github.com/pytorch/opacus/pull/625
    
    Differential Revision: D53292685

--- scripts/install_via_pip.sh
+++ scripts/install_via_pip.sh
@@ -35,7 +35,7 @@ while getopts 'ncdv:' flag; do
 
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+sudo apt-get -y update && sudo apt-get -y install yarn
 
 # yarn needs terminal info
 export TERM=xterm
