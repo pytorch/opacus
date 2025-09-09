31b65951cab805e8a08e8380d6d10b8069f39298
Huanyu Zhang <huanyuzhang@meta.com>
2025-09-09 08:48:14 -0700

    Validate/Replace nn.GRU by DPGRU
    Summary:
    To solve Github Issue [https://github.com/pytorch/opacus/issues/783].
    
    Add support of nn.GRU of the moduel validator
    
    Rollback Plan:
    
    Differential Revision: D82027020

---
+++ opacus/validators/gru.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import List
+
+import torch.nn as nn
+from opacus.layers import DPGRU
+
+from .errors import ShouldReplaceModuleError, UnsupportedModuleError
+from .utils import register_module_fixer, register_module_validator
+
+
+@register_module_validator(nn.GRU)
+def validate(module: nn.GRU) -> List[UnsupportedModuleError]:
+    return [
+        ShouldReplaceModuleError(
+            "We do not support nn.GRU because its implementation uses special "
+            "modules. We have written a GRU class that is a drop-in replacement "
+            "which is compatible with our Grad Sample hooks. Please run the recommended "
+            "replacement!"
+        )
+    ]
+
+
+@register_module_fixer(nn.GRU)
+def fix(module: nn.GRU) -> DPGRU:
+    dpgru = DPGRU(
+        input_size=module.input_size,
+        hidden_size=module.hidden_size,
+        num_layers=module.num_layers,
+        bias=module.bias,
+        batch_first=module.batch_first,
+        dropout=module.dropout,
+        bidirectional=module.bidirectional,
+        proj_size=module.proj_size,
+    )
+    dpgru.load_state_dict(module.state_dict())
+    return dpgru
