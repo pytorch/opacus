81282754de64d52c70f2a5325e193ee53173f565
Huanyu Zhang <huanyuzhang@meta.com>
2025-09-09 13:40:30 -0700

    Validate/Replace nn.GRU by DPGRU
    Summary:
    To solve Github Issue[https://github.com/pytorch/opacus/issues/783].
    
    Add support of nn.GRU of the moduel validator
    
    Reviewed By: iden-kalemaj
    
    Differential Revision: D82027020
    
    fbshipit-source-id: eb84efadd1a4e7048e4abd2c255f0fe219e74f74

---
+++ opacus/tests/validators/gru_test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import torch.nn as nn
+from opacus.layers import DPGRU
+from opacus.utils.module_utils import are_state_dict_equal
+from opacus.validators.errors import ShouldReplaceModuleError
+from opacus.validators.module_validator import ModuleValidator
+
+
+class GRUValidator_test(unittest.TestCase):
+    def setUp(self) -> None:
+        self.gru = nn.GRU(8, 4)
+        self.mv = ModuleValidator.VALIDATORS
+        self.mf = ModuleValidator.FIXERS
+
+    def test_validate(self) -> None:
+        val_gru = self.mv[type(self.gru)](self.gru)
+        self.assertEqual(len(val_gru), 1)
+        self.assertTrue(isinstance(val_gru[0], ShouldReplaceModuleError))
+
+    def test_fix(self) -> None:
+        fix_gru = self.mf[type(self.gru)](self.gru)
+        self.assertTrue(isinstance(fix_gru, DPGRU))
+        self.assertTrue(
+            are_state_dict_equal(self.gru.state_dict(), fix_gru.state_dict())
+        )
